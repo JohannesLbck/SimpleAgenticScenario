e4d0065e8c7ec11e4b5932e392f3c01014f26058
juergensmacbookpro <loebbeckejohannes@hotmail.de>
2026-07-21 10:44:49 +0200

    cleanup

--- MCP/eval_oo1_qwen.sh
+++
@@ -1,8 +0,0 @@
-for i in {0..9}; do
-  echo "This is iteration $i"
-  echo "Start"
-  date +'%Y-%m-%d %H:%M:%S.%N'
-  ./agent.rb 'Adjust the lighting based on standard guidelines for brightness during day/night and sleep/being awake. Also consider that the light should be turned on whenever it is dark and I want to go somewhere! Make sure this is done for the next 72 seconds which corresponds to 12 hours in simulated time (check for the conditions every second - i.e., every 10 minutes in simulated time)! Log the tools you use along with their parameters (apart from the logging tool itself)!' "oo1_qwen_run_$i" 'Qwen/Qwen3.6-27B'
-  echo "End"
-  date +'%Y-%m-%d %H:%M:%S.%N'
-done
--- MCP/eval_qwen.sh
+++
@@ -1,2 +0,0 @@
-./do_it.sh eval_oo1_qwen
-./do_it.sh eval_oo3_qwen
